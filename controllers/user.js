3cd04468b921dc20ce23d90c5a9bdeadd3dbdec7
EunJeong Lee <sssver28@gmail.com>
2021-04-08 19:04:58 +0900

    add /user route

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const axios = require('axios');
-const { User: UserModel } = require('../models');
+const { User: UserModel, Item: ItemModel } = require('../models');
 const clientID = process.env.KAKAO_CLIENT_ID;
 const clientSecret = process.env.KAKAO_CLIENT_SECRET;
 const redirectURL = process.env.KAKAO_REDIRECT_URL;
 
 module.exports = {
+  // 카카오 오어스 로그인, 강제회원가입
   'oauth': async (req, res) => {
-    // console.log(req.body.authorizationCode); // 코드 들어옴
+    const { authorizationCode } = req.body;
 
     axios({
       method: 'post',
@@ -19,7 +20,7 @@ module.exports = {
         grant_type: 'authorization_code',
         client_id: clientID,
         redirect_uri: redirectURL,
-        code: req.body.authorizationCode,
+        code: authorizationCode,
         client_secret: clientSecret,
       }
     }).then((response) => {
@@ -52,13 +53,85 @@ module.exports = {
       res.status(500).json({ 'message': 'Fail to login' });
     });
   },
+
+  // 닉네임 변경
   'name': async (req, res) => {
-    console.log('name', req.body);
+    const { userId, name } = req.body;
+    await UserModel.update({ name: name }, {
+      where: {
+        id: userId
+      }
+    })
+      .then(() => {
+        res.status(200).json({ 'message': 'ok' });
+      }).catch(() => {
+        res.status(500).json({ 'message': 'Fail to update name' });
+      });
   },
+
+  // 입찰에 참여한 물품 조회
   'getBuyerItems': async (req, res) => {
-    console.log('myauctionBuyer', req.body);
+    const { userId } = req.body;
+    await UserModel.findAll({
+      where: {
+        id: userId,
+      },
+      include: [
+        {
+          model: ItemModel,
+          required: true,
+          as: 'ItemB',
+          through: {
+            attributes: ['UserId', 'ItemId']
+          }
+        }
+      ]
+    })
+      .then((result) => {
+        if (result.length) {
+          const items = result[0].dataValues.ItemB.map((item) => {
+            return item.dataValues;
+          });
+          res.status(200).json({ items });
+        } else {
+          const items = [];
+          res.status(200).json({ items });
+        }
+      }).catch(() => {
+        res.status(500).json({ 'message': 'Fail to load data from database' });
+      });
   },
+
+  // 경매에 내놓은 물품 조회
   'getSellerItems': async (req, res) => {
-    console.log('myauctionSeller', req.body);
+    const { userId } = req.body;
+    await UserModel.findAll({
+      where: {
+        id: userId,
+      },
+      include: [
+        {
+          model: ItemModel,
+          required: true,
+          as: 'Item',
+          through: {
+            attributes: ['UserId', 'ItemId']
+          }
+        }
+      ]
+    })
+      .then((result) => {
+        if (result.length) {
+          const items = result[0].dataValues.Item.map((item) => {
+            return item.dataValues;
+          });
+          res.status(200).json({ items });
+        } else {
+          const items = [];
+          res.status(200).json({ items });
+        }
+      }).catch(() => {
+        res.status(500).json({ 'message': 'Fail to load data from database' });
+      });
   },
 };
