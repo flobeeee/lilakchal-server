0c48ab28908b6855c114b75a3ae2f9a6f13a98a2
EunJeong Lee <sssver28@gmail.com>
2021-04-14 15:07:33 +0900

    limit timeOver items

--- controllers/search.js
+++ controllers/search.js
@@ -6,6 +6,7 @@ module.exports = {
   'keyword': async (req, res) => {
     // console.log(req.query); // {keyword: '맥북, city: '서울시 마포구', offset: 1}
     const { keyword, city, offset } = req.query;
+    const today = new Date();
     // 검색어로 아이템 이름 검색
     if (keyword) {
       ItemModel.findAll({
@@ -21,7 +22,12 @@ module.exports = {
             description: {
               [Op.like]: '%' + keyword + '%'
             }
-          }]
+          }],
+          [Op.and]: {
+            endTime: {
+              [Op.gt]: today,
+            }
+          }
         }
       })
         .then((result) => {
@@ -41,9 +47,15 @@ module.exports = {
     }
     // 랜딩페이지에서 다음 페이지로 이동하는 순간 지역에 대한 정보가 들어 올 예정
     else {
+      const today = new Date();
       ItemModel.findAll({
         offset: Number(offset) || 0,
         limit: 5,
+        where: {
+          endTime: {
+            [Op.gt]: today,
+          }
+        },
       })
         .then((result) => {
           if (result.length) {
