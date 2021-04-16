a265148f8b8f63024dadaf8c691bcb0032f89807
ImHyeLim1209 <ihl279090@gmail.com>
2021-04-16 14:27:00 +0900

    modify saving photo name by create name

--- routes/auction.js
+++ routes/auction.js
@@ -1,22 +1,7 @@
 const express = require('express');
 const auctionController = require('../controllers/auction');
 const router = express.Router();
-const multer = require('multer');
-
-const path = require('path');
-
-const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, cb) {
-      cb(null, 'uploads/');
-    },
-    filename(req, file, cb) {
-      const ext = path.extname(file.originalname);
-      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
-    },
-  }),
-  limits: { fileSize: 5 * 1024 * 1024 },
-});
+const upload = require('../modules/upload');
 
 router.post('/register', upload.single('file'), auctionController.register);
 
