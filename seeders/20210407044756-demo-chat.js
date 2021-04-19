0a508b6d32d5025213b6a8ad067a3ab19277a741
EunJeong Lee <sssver28@gmail.com>
2021-04-19 15:16:51 +0900

    fix endTime

--- seeders/20210407044756-demo-chat.js
+++ seeders/20210407044756-demo-chat.js
@@ -19,6 +19,48 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
+      {
+        userId: users[0][1].id,
+        itemId: items[0][1].id,
+        message: '시간 언제가 좋으세요?',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userId: users[0][2].id,
+        itemId: items[0][1].id,
+        message: '이번주 주말 괜찮으신가요 ?',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userId: users[0][2].id,
+        itemId: items[0][2].id,
+        message: '안녕하세요~~',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userId: users[0][3].id,
+        itemId: items[0][2].id,
+        message: '네 안녕하세여 ㅎㅎ',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userId: users[0][3].id,
+        itemId: items[0][3].id,
+        message: '택배로 배송해도 될까요?',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userId: users[0][0].id,
+        itemId: items[0][3].id,
+        message: '저는 직거래가 편한데 ..',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
     ]);
   },
 
